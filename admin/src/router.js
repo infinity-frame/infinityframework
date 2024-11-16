db49d061d21bed8436829794dd782e3aa7de50cd
vojtechhabes <voj.hab@proton.me>
2024-11-16 13:35:46 +0100

    Add auth store

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,16 +1,39 @@
 import { createRouter, createWebHistory } from "vue-router";
+import authStore from "./stores/auth";
 
 const routes = [
   {
-    path: "/",
+    path: "/login",
     name: "Login",
     component: () => import("./views/Login.vue"),
+    meta: {
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/",
+    name: "Dashboard",
+    component: () => import("./views/Dashboard.vue"),
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  if (to.meta.requiresAuth && !authStore.currentUser) {
+    next("/login");
+  } else if (!to.meta.requiresAuth && authStore.currentUser) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ admin/src/stores/auth.js
@@ -0,0 +1,9 @@
+import { reactive } from "vue";
+
+const authStore = reactive({
+  currentUser: null,
+
+  // Functions related to authentication
+});
+
+export default authStore;
