1753543159dc2d4b4ad2122c7afefa3daec782a2
vojtechhabes <voj.hab@proton.me>
2024-12-13 21:16:46 +0100

    Start working on basic design and layout

--- src/admin/src/main.js
+++ src/admin/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./components/App.vue";
 import router from "./router";
+import "@infinity-frame/infinitycomponent/styles/main.scss";
 
 createApp(App).use(router).mount("#app");
--- src/admin/src/views/Login.vue
+++ src/admin/src/views/Login.vue
@@ -1,3 +1,32 @@
+<script setup>
+import {
+  Button,
+  CenterBoxLayout,
+  Actions,
+  TextInput,
+} from "@infinity-frame/infinitycomponent";
+</script>
+
 <template>
-  <h1>Přihlášení</h1>
+  <CenterBoxLayout title="Přihlášení">
+    <p>Vítejte v administračním systému InfinityFrameworku!</p>
+    <form>
+      <TextInput name="username" label="Email" icon="mail" required />
+      <TextInput
+        name="password"
+        label="Heslo"
+        type="password"
+        icon="lock"
+        required
+      />
+      <Actions>
+        <Button
+          color="primary"
+          label="Přihlásit se"
+          icon="login"
+          type="submit"
+        />
+      </Actions>
+    </form>
+  </CenterBoxLayout>
 </template>
