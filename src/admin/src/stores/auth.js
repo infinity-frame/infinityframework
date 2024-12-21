ff8bddf45c581bb21d14980364a06bc92bf98133
vojtechhabes <voj.hab@proton.me>
2024-12-21 19:55:11 +0100

    Add simulated auth functions

--- src/admin/src/stores/auth.js
+++ src/admin/src/stores/auth.js
@@ -3,7 +3,53 @@ import { reactive } from "vue";
 const authStore = reactive({
   currentUser: null,
 
-  // Functions related to authentication
+  async login(email, password) {
+    // TODO: Send request to backend when it is ready.
+
+    // Simulate login
+    this.currentUser = {
+      email,
+    };
+
+    localStorage.setItem("refreshToken", "dummyRefreshToken");
+  },
+
+  async logout() {
+    // TODO: Send request to backend when it is ready.
+
+    // Simulate logout
+    this.currentUser = null;
+    localStorage.setItem("refreshToken", "dummyRefreshToken");
+  },
+
+  async changePassword(oldPassword, newPassword) {
+    // TODO: Send request to backend when it is ready.
+
+    // Simulate logout after password change
+    this.currentUser = null;
+    localStorage.removeItem("refreshToken");
+  },
+
+  async changeEmail(newEmail) {
+    // TODO: Send request to backend when it is ready.
+
+    // Simulate email change
+    this.currentUser.email = newEmail;
+  },
+
+  async refreshSession() {
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (!refreshToken) return;
+
+    // TODO: Send request to backend when it is ready.
+
+    // Simulate session refresh
+    this.currentUser = {
+      email: "ahojky@ahojky.cz",
+    };
+  },
 });
+
+authStore.refreshSession();
 
 export default authStore;
