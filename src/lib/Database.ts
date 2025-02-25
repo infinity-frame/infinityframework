5b955ba3e8585fa8c412038552c1a278be2abc4a
OndrejDudacek <ondrej.dudacek@gmail.com>
2025-02-25 09:00:16 +0100

    Fix if-users collection being possibly non-existent, add concurrently

--- src/lib/Database.ts
+++ src/lib/Database.ts
@@ -3,26 +3,26 @@ import { Logger } from "pino";
 import { Manifest } from "./Manifest.js";
 
 export interface Database {
-  db: Db;
-  closeClient: () => Promise<void>;
+	db: Db;
+	closeClient: () => Promise<void>;
 }
 
 export async function DbFactory(
-  manifest: Manifest,
-  logger: Logger
+	manifest: Manifest,
+	logger: Logger
 ): Promise<Database> {
-  const uri: string = manifest.dbUri;
+	const uri: string = manifest.dbUri;
 
-  logger.info(`Initializing connection to db ${uri}`);
-  const client = new MongoClient(uri);
-  await client.connect();
+	logger.info(`Initializing connection to db ${uri}`);
+	const client = new MongoClient(uri);
+	await client.connect();
 
-  const adminDb = client.db("admin");
-  await adminDb.command({ ping: 1 });
-  logger.info(`Connection to db ${uri} successfully initialized`);
+	const adminDb = client.db("admin");
+	await adminDb.command({ ping: 1 });
+	logger.info(`Connection to db ${uri} successfully initialized`);
 
-  const db = client.db("if");
+	const db = client.db("if");
 
-  const closeClient = async () => await client.close();
-  return { db, closeClient };
+	const closeClient = async () => await client.close();
+	return { db, closeClient };
 }
