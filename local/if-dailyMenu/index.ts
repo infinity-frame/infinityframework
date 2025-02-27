967f9841c6ca7637e736f562d9925988df97d799
OndrejDudacek <ondrej.dudacek@gmail.com>
2025-02-27 12:54:03 +0100

    Finish dailyMenu backend

--- local/if-dailyMenu/index.ts
+++ local/if-dailyMenu/index.ts
@@ -1,16 +1,27 @@
 import express, { NextFunction, Request, Response } from "express";
 import { ModuleInitializer } from "../../dist/types.js";
-import { stringify } from "querystring";
+import { ObjectId } from "mongodb";
 
 type Item = {
 	name: string;
 	price: number;
 };
 
-type DailyMenu = {
-	id: string;
+type CreateDailyMenu = {
 	date: string;
 	menu: Item[];
+};
+
+type DailyMenu = CreateDailyMenu & {
+	_id: string;
+};
+
+const isItemArray = (value: unknown): value is Item[] => {
+	if (!Array.isArray(value)) return false;
+	return value.every(
+		(item) =>
+			typeof item.name === "string" && typeof item.price === "number"
+	);
 };
 
 const dailyMenus: DailyMenu[] = [];
@@ -23,32 +34,55 @@ const ModuleInitializer: ModuleInitializer = async (context) => {
 		context.logger.info(context.app.modules);
 	});
 
-	router.get("/ping", (req: Request, res: Response, next: NextFunction) => {
+	router.get("/ping", (req: Request, res: Response) => {
 		res.send("pong");
 	});
 
 	router.post("/menu", async (req: Request, res: Response) => {
-		if (
-			typeof req.body.date !== "string" ||
-			typeof req.body.menu !== "object"
-		) {
+		if (typeof req.body.date !== "string" || !isItemArray(req.body.menu)) {
 			res.status(400).send({ error: "Invalid request body" });
 			return;
 		}
 
-		const dailyMenu: DailyMenu = {
-			id: Math.random().toString(36),
-			date: req.body.date,
-			menu: req.body.menu,
-		};
+		const dailyMenu = req.body as CreateDailyMenu;
 
-		dailyMenus.push(dailyMenu);
+		const result = await context.collections.dailyMenus.insertOne({
+			date: dailyMenu.date,
+			menu: dailyMenu.menu,
+		} as CreateDailyMenu);
 
-		res.status(201).json(dailyMenu);
+		const insertedDailyMenu = await context.collections.dailyMenus.findOne({
+			_id: result.insertedId,
+		});
+
+		res.status(201).json(insertedDailyMenu);
 	});
 
-	router.get("/menu", (req: Request, res: Response, next: NextFunction) => {
-		res.status(200).json(dailyMenus);
+	router.get("/menu", async (req: Request, res: Response) => {
+		const dailyMenus = await context.collections.dailyMenus
+			.find({})
+			.toArray();
+
+		res.send(dailyMenus);
+	});
+
+	router.delete("/menu/:id", async (req: Request, res: Response) => {
+		const dailyMenuId = req.params.id;
+
+		if (!ObjectId.isValid(dailyMenuId)) {
+			res.status(400).send({ error: "Invalid ID" });
+			return;
+		}
+
+		const result = await context.collections.dailyMenus.deleteOne({
+			_id: new ObjectId(dailyMenuId),
+		});
+
+		if (result.deletedCount === 0) {
+			res.status(404).send({ error: "Daily menu not found" });
+		}
+
+		res.status(204).end();
 	});
 
 	return {
