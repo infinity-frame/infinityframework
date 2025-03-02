4deec3ca1dab8816756074f0a9435a68a278ef93
OndrejDudacek <ondrej.dudacek@gmail.com>
2025-03-02 13:35:55 +0100

    Finish dailyMenu module with its frontend

--- local/if-dailyMenu/frontend/src/App.vue
+++ local/if-dailyMenu/frontend/src/App.vue
@@ -1,48 +1,72 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import useApi from "./useApi";
 import {
 	Heading,
 	Box,
-	RichTextEditor,
 	TextInput,
-	DropdownInput,
 	Button,
-	Actions,
 	NormalLayout,
 	ColumnsLayout,
 	Icon,
 } from "@infinity-frame/infinitycomponent";
 
 const { apiUrl, authFetch } = useApi();
-console.log(apiUrl);
+const moduleApiUrl = ref("");
 
-const moduleApiUrl = apiUrl + "/api/infinityframe/dailyMenu";
+/**
+ * Watch for changes in the apiUrl and update moduleApiUrl accordingly.
+ * Fetch daily menus when apiUrl changes.
+ */
+watch(apiUrl, (newApiUrl) => {
+	if (newApiUrl) {
+		moduleApiUrl.value = newApiUrl + "/api/infinityframe/dailyMenu/menu";
+		fetchDailyMenus();
+	}
+});
 
 const dailyMenus = ref([]);
 const newDate = ref("");
 const newMenu = ref([]);
 const newItems = ref({
-	item: "",
-	price: "",
+	name: "",
+	price: null,
 });
 
+/**
+ * Fetch daily menus from the API.
+ * @async
+ */
 const fetchDailyMenus = async () => {
-	const response = await authFetch(moduleApiUrl, { method: "GET" });
+	if (!moduleApiUrl.value) return;
+
+	const response = await authFetch(moduleApiUrl.value, { method: "GET" });
 	if (response.ok) {
 		dailyMenus.value = await response.json();
+		console.log(dailyMenus.value);
 	} else {
 		alert("Něco se pokazilo.");
 	}
 };
 
+/**
+ * Save a new menu to the API.
+ * @async
+ */
 const saveMenu = async () => {
 	if (!newDate.value || !newMenu.value.length) {
 		alert("Vyplňte všechny údaje.");
 		return;
 	}
 
-	const response = await authFetch(moduleApiUrl, {
+	console.log(
+		JSON.stringify({
+			date: newDate.value,
+			menu: newMenu.value,
+		})
+	);
+
+	const response = await authFetch(moduleApiUrl.value, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -54,7 +78,6 @@ const saveMenu = async () => {
 	});
 
 	if (response.ok) {
-		alert("Menu bylo úspěšně uloženo.");
 		newDate.value = "";
 		newMenu.value = [];
 		fetchDailyMenus();
@@ -63,40 +86,118 @@ const saveMenu = async () => {
 	}
 };
 
+/**
+ * Add a new item to the menu.
+ */
 const addItem = () => {
+	if (!newItems.value.name || !newItems.value.price) {
+		alert("Vyplňte všechny údaje položky.");
+		return;
+	}
+
+	try {
+		newItems.value.price = parseFloat(newItems.value.price);
+	} catch (e) {
+		alert("Cena musí být číslo.");
+		return;
+	}
+
 	newMenu.value.push(newItems.value);
-	newItems.value = { item: "", price: "" };
+	newItems.value = { name: "", price: null };
 };
 
-console.log(onMounted(fetchDailyMenus));
+/**
+ * Delete a menu by its ID.
+ * @async
+ * @param {string} id - The ID of the menu to delete.
+ */
+const deleteMenu = async (id) => {
+	if (!confirm("Opravdu chcete smazat toto menu?")) {
+		return;
+	}
+
+	const response = await authFetch(moduleApiUrl.value + `/${id}`, {
+		method: "DELETE",
+	});
+
+	if (response.ok) {
+		fetchDailyMenus();
+	} else {
+		alert("Něco se pokazilo.");
+	}
+};
+
+/**
+ * Fetch daily menus on component mount.
+ */
+onMounted(() => {
+	if (apiUrl.value) {
+		moduleApiUrl.value = apiUrl.value + "/api/infinityframe/dailyMenu";
+		fetchDailyMenus();
+	}
+});
 </script>
 
 <template>
 	<NormalLayout>
-		<Heading level="2">Denní menu</Heading>
+		<Heading :semanticLevel="2">Denní menu</Heading>
 
-		<Box title="Nové menu">
-			<TextInput v-model="newDate" label="Datum" />
+		<Heading :semanticLevel="3">Nové menu</Heading>
+		<TextInput
+			v-model="newDate"
+			placeholder="Datum ve formátu DD.MM.RRRR"
+		/>
 
-			<Box :title="`Položky v menu na ${newDate || 'vybraný den'}`">
+		<Box :title="`Položky v menu na ${newDate || 'vybraný den'}`">
+			<ul>
+				<li v-for="(item, i) in newMenu" :key="i">
+					{{ item.name }} ............. {{ item.price }} Kč
+					<Icon @click="newMenu.splice(i, 1)" name="delete"></Icon>
+				</li>
+			</ul>
+			<ColumnsLayout :columns="3">
+				<TextInput v-model="newItems.name" placeholder="Věc" />
+				<TextInput v-model="newItems.price" placeholder="Cena" />
+				<Button @click="addItem()" label="Přidat položku"></Button>
+			</ColumnsLayout>
+		</Box>
+		<Button label="Uložit menu" @click="saveMenu"></Button>
+
+		<Heading :semanticLevel="3">Existující meny</Heading>
+		<ColumnsLayout :columns="2">
+			<Box
+				v-for="(menu, i) in dailyMenus"
+				:key="i"
+				:title="menu.date"
+				:class="`today-${menu.today}`"
+			>
 				<ul>
-					<li v-for="(item, i) in newMenu" :key="i">
-						{{ item.item }} ............. {{ item.price }}
-						<Icon
-							@click="newMenu.splice(i, 1)"
-							name="delete"
-						></Icon>
+					<li v-for="(item, i) in menu.menu" :key="i">
+						{{ item.name }} ............. {{ item.price }}
 					</li>
 				</ul>
-				<ColumnsLayout :columns="3">
-					<TextInput v-model="newItems.item" placeholder="Věc" />
-					<TextInput v-model="newItems.price" placeholder="Cena" />
-					<Button @click="addItem()" label="Přidat položku"></Button>
-				</ColumnsLayout>
+				<Button
+					color="danger"
+					icon="delete"
+					@click="deleteMenu(menu._id)"
+					label="Smazat"
+				></Button>
 			</Box>
-			<Button label="Uložit menu" @click="saveMenu"></Button>
-		</Box>
+		</ColumnsLayout>
 	</NormalLayout>
 </template>
 
-<style scoped></style>
+<style>
+@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+@import "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
+
+.box.today-true {
+	background-color: #e5ffda;
+}
+
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+</style>
--- local/if-dailyMenu/index.ts
+++ local/if-dailyMenu/index.ts
@@ -1,92 +1,109 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { ModuleInitializer } from "../../dist/types.js";
 import { ObjectId } from "mongodb";
+import cron from "node-cron";
+import { format, isToday, parse } from "date-fns";
 
 type Item = {
-  name: string;
-  price: number;
+	name: string;
+	price: number;
 };
 
 type CreateDailyMenu = {
-  date: string;
-  menu: Item[];
-};
-
-type DailyMenu = CreateDailyMenu & {
-  _id: string;
+	date: string;
+	menu: Item[];
+	today: boolean;
 };
 
 const isItemArray = (value: unknown): value is Item[] => {
-  if (!Array.isArray(value)) return false;
-  return value.every(
-    (item) => typeof item.name === "string" && typeof item.price === "number"
-  );
+	if (!Array.isArray(value)) return false;
+	return value.every(
+		(item) =>
+			typeof item.name === "string" && typeof item.price === "number"
+	);
 };
 
-const dailyMenus: DailyMenu[] = [];
-
 const moduleInitializer: ModuleInitializer = async (context) => {
-  const router = express.Router();
-  router.use(express.json());
+	const router = express.Router();
+	router.use(express.json());
 
-  context.app.events.addListener("initialized", () => {
-    context.logger.info(context.app.modules);
-  });
+	context.app.events.addListener("initialized", () => {
+		context.logger.info(context.app.modules);
+	});
 
-  router.get("/ping", (req: Request, res: Response) => {
-    res.send("pong");
-  });
+	router.get("/ping", (req: Request, res: Response) => {
+		res.send("pong");
+	});
 
-  router.post("/menu", async (req: Request, res: Response) => {
-    if (typeof req.body.date !== "string" || !isItemArray(req.body.menu)) {
-      res.status(400).send({ error: "Invalid request body" });
-      return;
-    }
+	router.post("/menu", async (req: Request, res: Response) => {
+		if (typeof req.body.date !== "string" || !isItemArray(req.body.menu)) {
+			res.status(400).send({ error: "Invalid request body" });
+			return;
+		}
 
-    const dailyMenu = req.body as CreateDailyMenu;
+		const dailyMenu = req.body as CreateDailyMenu;
 
-    const result = await context.collections.dailyMenus.insertOne({
-      date: dailyMenu.date,
-      menu: dailyMenu.menu,
-    } as CreateDailyMenu);
+		let checkToday = false;
+		if (dailyMenu.date === format(new Date(), "dd.MM.yyyy")) {
+			checkToday = true;
+		}
 
-    const insertedDailyMenu = await context.collections.dailyMenus.findOne({
-      _id: result.insertedId,
-    });
+		const result = await context.collections.dailyMenus.insertOne({
+			date: dailyMenu.date,
+			menu: dailyMenu.menu,
+			today: checkToday,
+		} as CreateDailyMenu);
 
-    res.status(201).json(insertedDailyMenu);
-  });
+		const insertedDailyMenu = await context.collections.dailyMenus.findOne({
+			_id: result.insertedId,
+		});
 
-  router.get("/menu", async (req: Request, res: Response) => {
-    const dailyMenus = await context.collections.dailyMenus.find({}).toArray();
+		res.status(201).json(insertedDailyMenu);
+	});
 
-    res.send(dailyMenus);
-  });
+	router.get("/menu", async (req: Request, res: Response) => {
+		const dailyMenus = await context.collections.dailyMenus
+			.find({})
+			.toArray();
 
-  router.delete("/menu/:id", async (req: Request, res: Response) => {
-    const dailyMenuId = req.params.id;
+		res.status(200).send(dailyMenus);
+	});
 
-    if (!ObjectId.isValid(dailyMenuId)) {
-      res.status(400).send({ error: "Invalid ID" });
-      return;
-    }
+	router.delete("/menu/:id", async (req: Request, res: Response) => {
+		const dailyMenuId = req.params.id;
 
-    const result = await context.collections.dailyMenus.deleteOne({
-      _id: new ObjectId(dailyMenuId),
-    });
+		if (!ObjectId.isValid(dailyMenuId)) {
+			res.status(400).send({ error: "Invalid ID" });
+			return;
+		}
 
-    if (result.deletedCount === 0) {
-      res.status(404).send({ error: "Daily menu not found" });
-    }
+		const result = await context.collections.dailyMenus.deleteOne({
+			_id: new ObjectId(dailyMenuId),
+		});
 
-    res.status(204).end();
-  });
+		if (result.deletedCount === 0) {
+			res.status(404).send({ error: "Daily menu not found" });
+		}
 
-  return {
-    router,
-    methods: {},
-    contexts: {},
-  };
+		res.status(204);
+	});
+
+	cron.schedule("0 0 * * *", async () => {
+		const todayDate = format(new Date(), "dd.MM.yyyy");
+
+		const result = await context.collections.dailyMenus.updateMany(
+			{ date: todayDate },
+			{ $set: { today: true } }
+		);
+
+		context.logger.info(`Updated ${result.modifiedCount} menus for today.`);
+	});
+
+	return {
+		router,
+		methods: {},
+		contexts: {},
+	};
 };
 
 export default moduleInitializer;
