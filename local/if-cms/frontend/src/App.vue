9e49826143ad3bee63efcd2fda867836baa9cfd7
OndrejDudacek <ondrej.dudacek@gmail.com>
2025-03-25 09:00:38 +0100

    Update asset script references and improve App.vue structure

--- local/if-cms/frontend/src/App.vue
+++ local/if-cms/frontend/src/App.vue
@@ -1,110 +1,109 @@
 <script setup>
-import { ref, watch, onMounted } from "vue";
-import useApi from "./useApi";
-import {
-	Heading,
-	Box,
-	RichTextEditor,
-	TextInput,
-	Button,
-	Actions,
-	NormalLayout,
-} from "@infinity-frame/infinitycomponent";
+	import { ref, watch, onMounted } from "vue";
+	import useApi from "./useApi";
+	import {
+		Heading,
+		Box,
+		RichTextEditor,
+		TextInput,
+		Button,
+		Actions,
+		NormalLayout,
+	} from "@infinity-frame/infinitycomponent";
 
-const { apiUrl, authFetch } = useApi();
-const moduleApiUrl = ref("");
+	const { apiUrl, authFetch } = useApi();
+	const moduleApiUrl = ref("");
 
-/**
- * Watch for changes in the apiUrl and update moduleApiUrl accordingly.
- * Fetch content blocks when apiUrl changes.
- */
-watch(apiUrl, (newApiUrl) => {
-	if (newApiUrl) {
-		moduleApiUrl.value = newApiUrl + "/api/infinityframe/cms/contentBlock";
-		fetchContentBlocks();
-	}
-});
-
-const newElementId = ref("");
-const newDescription = ref("");
-const newContent = ref("");
-
-const contentBlocks = ref([]);
-
-/**
- * Fetch content blocks from the API.
- * @async
- */
-const fetchContentBlocks = async () => {
-	if (!moduleApiUrl.value) return;
-
-	const response = await authFetch(moduleApiUrl.value, { method: "GET" });
-	if (response.ok) {
-		contentBlocks.value = await response.json();
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
-
-/**
- * Save a new content block to the API.
- * @async
- */
-const saveContentBlock = async () => {
-	if (!newElementId.value || !newContent.value) {
-		alert("Vyplňte povinné údaje.");
-		return;
-	}
-
-	const response = await authFetch(moduleApiUrl.value, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			elementId: newElementId.value,
-			description: newDescription.value,
-			content: newContent.value,
-		}),
+	/**
+	 * Watch for changes in the apiUrl and update moduleApiUrl accordingly.
+	 * Fetch content blocks when apiUrl changes.
+	 */
+	watch(apiUrl, (newApiUrl) => {
+		if (newApiUrl) {
+			moduleApiUrl.value = newApiUrl + "/api/infinityframe/cms/contentBlock";
+			fetchContentBlocks();
+		}
 	});
 
-	if (response.ok) {
-		fetchContentBlocks();
-		newElementId.value = "";
-		newDescription.value = "";
-		newContent.value = "";
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
+	const newElementId = ref("");
+	const newDescription = ref("");
+	const newContent = ref("");
 
-/**
- * Delete a content block by its ID.
- * @async
- * @param {string} blockId - The ID of the content block to delete.
- */
-const deleteContentBlock = async (blockId) => {
-	const response = await authFetch(moduleApiUrl.value + "/" + blockId, {
-		method: "DELETE",
+	const contentBlocks = ref([]);
+
+	/**
+	 * Fetch content blocks from the API.
+	 * @async
+	 */
+	const fetchContentBlocks = async () => {
+		if (!moduleApiUrl.value) return;
+
+		const response = await authFetch(moduleApiUrl.value, { method: "GET" });
+		if (response.ok) {
+			contentBlocks.value = await response.json();
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Save a new content block to the API.
+	 * @async
+	 */
+	const saveContentBlock = async () => {
+		if (!newElementId.value || !newContent.value) {
+			alert("Vyplňte povinné údaje.");
+			return;
+		}
+
+		const response = await authFetch(moduleApiUrl.value, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				elementId: newElementId.value,
+				description: newDescription.value,
+				content: newContent.value,
+			}),
+		});
+
+		if (response.ok) {
+			fetchContentBlocks();
+			newElementId.value = "";
+			newDescription.value = "";
+			newContent.value = "";
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Delete a content block by its ID.
+	 * @async
+	 * @param {string} blockId - The ID of the content block to delete.
+	 */
+	const deleteContentBlock = async (blockId) => {
+		const response = await authFetch(moduleApiUrl.value + "/" + blockId, {
+			method: "DELETE",
+		});
+
+		if (response.ok) {
+			fetchContentBlocks();
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Fetch content blocks on component mount.
+	 */
+	onMounted(() => {
+		if (apiUrl.value) {
+			moduleApiUrl.value = apiUrl.value + "/api/infinityframe/cms/contentBlock";
+			fetchContentBlocks();
+		}
 	});
-
-	if (response.ok) {
-		fetchContentBlocks();
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
-
-/**
- * Fetch content blocks on component mount.
- */
-onMounted(() => {
-	if (apiUrl.value) {
-		moduleApiUrl.value =
-			apiUrl.value + "/api/infinityframe/cms/contentBlock";
-		fetchContentBlocks();
-	}
-});
 </script>
 
 <template>
@@ -114,7 +113,7 @@ onMounted(() => {
 		<Box title="Nový obsahový blok">
 			<TextInput
 				v-model="newElementId"
-				placeholder="Identifikátor elemntu"
+				placeholder="Identifikátor elementu"
 				icon="badge"
 			/>
 			<TextInput
@@ -149,12 +148,12 @@ onMounted(() => {
 </template>
 
 <style>
-@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
-@import "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
+	@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+	@import "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
 
-* {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
-}
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
 </style>
--- local/if-dailyMenu/frontend/src/App.vue
+++ local/if-dailyMenu/frontend/src/App.vue
@@ -1,142 +1,142 @@
 <script setup>
-import { onMounted, ref, watch } from "vue";
-import useApi from "./useApi";
-import {
-	Heading,
-	Box,
-	TextInput,
-	Button,
-	NormalLayout,
-	ColumnsLayout,
-	Icon,
-	Actions,
-} from "@infinity-frame/infinitycomponent";
+	import { onMounted, ref, watch } from "vue";
+	import useApi from "./useApi";
+	import {
+		Heading,
+		Box,
+		TextInput,
+		Button,
+		NormalLayout,
+		ColumnsLayout,
+		Icon,
+		Actions,
+	} from "@infinity-frame/infinitycomponent";
 
-const { apiUrl, authFetch } = useApi();
-const moduleApiUrl = ref("");
+	const { apiUrl, authFetch } = useApi();
+	const moduleApiUrl = ref("");
 
-/**
- * Watch for changes in the apiUrl and update moduleApiUrl accordingly.
- * Fetch daily menus when apiUrl changes.
- */
-watch(apiUrl, (newApiUrl) => {
-	if (newApiUrl) {
-		moduleApiUrl.value = newApiUrl + "/api/infinityframe/dailyMenu/menu";
-		fetchDailyMenus();
-	}
-});
-
-const dailyMenus = ref([]);
-const newDate = ref("");
-const newMenu = ref([]);
-const newItems = ref({
-	name: "",
-	price: null,
-});
-
-/**
- * Fetch daily menus from the API.
- * @async
- */
-const fetchDailyMenus = async () => {
-	if (!moduleApiUrl.value) return;
-
-	const response = await authFetch(moduleApiUrl.value, { method: "GET" });
-	if (response.ok) {
-		dailyMenus.value = await response.json();
-		console.log(dailyMenus.value);
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
-
-/**
- * Save a new menu to the API.
- * @async
- */
-const saveMenu = async () => {
-	if (!newDate.value || !newMenu.value.length) {
-		alert("Vyplňte všechny údaje.");
-		return;
-	}
-
-	console.log(
-		JSON.stringify({
-			date: newDate.value,
-			menu: newMenu.value,
-		})
-	);
-
-	const response = await authFetch(moduleApiUrl.value, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			date: newDate.value,
-			menu: newMenu.value,
-		}),
+	/**
+	 * Watch for changes in the apiUrl and update moduleApiUrl accordingly.
+	 * Fetch daily menus when apiUrl changes.
+	 */
+	watch(apiUrl, (newApiUrl) => {
+		if (newApiUrl) {
+			moduleApiUrl.value = newApiUrl + "/api/infinityframe/dailyMenu/menu";
+			fetchDailyMenus();
+		}
 	});
 
-	if (response.ok) {
-		newDate.value = "";
-		newMenu.value = [];
-		fetchDailyMenus();
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
-
-/**
- * Add a new item to the menu.
- */
-const addItem = () => {
-	if (!newItems.value.name || !newItems.value.price) {
-		alert("Vyplňte všechny údaje položky.");
-		return;
-	}
-
-	try {
-		newItems.value.price = parseFloat(newItems.value.price);
-	} catch (e) {
-		alert("Cena musí být číslo.");
-		return;
-	}
-
-	newMenu.value.push(newItems.value);
-	newItems.value = { name: "", price: null };
-};
-
-/**
- * Delete a menu by its ID.
- * @async
- * @param {string} id - The ID of the menu to delete.
- */
-const deleteMenu = async (id) => {
-	if (!confirm("Opravdu chcete smazat toto menu?")) {
-		return;
-	}
-
-	const response = await authFetch(moduleApiUrl.value + `/${id}`, {
-		method: "DELETE",
+	const dailyMenus = ref([]);
+	const newDate = ref("");
+	const newMenu = ref([]);
+	const newItems = ref({
+		name: "",
+		price: null,
 	});
 
-	if (response.ok) {
-		fetchDailyMenus();
-	} else {
-		alert("Něco se pokazilo.");
-	}
-};
+	/**
+	 * Fetch daily menus from the API.
+	 * @async
+	 */
+	const fetchDailyMenus = async () => {
+		if (!moduleApiUrl.value) return;
 
-/**
- * Fetch daily menus on component mount.
- */
-onMounted(() => {
-	if (apiUrl.value) {
-		moduleApiUrl.value = apiUrl.value + "/api/infinityframe/dailyMenu";
-		fetchDailyMenus();
-	}
-});
+		const response = await authFetch(moduleApiUrl.value, { method: "GET" });
+		if (response.ok) {
+			dailyMenus.value = await response.json();
+			console.log(dailyMenus.value);
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Save a new menu to the API.
+	 * @async
+	 */
+	const saveMenu = async () => {
+		if (!newDate.value || !newMenu.value.length) {
+			alert("Vyplňte všechny údaje.");
+			return;
+		}
+
+		console.log(
+			JSON.stringify({
+				date: newDate.value,
+				menu: newMenu.value,
+			})
+		);
+
+		const response = await authFetch(moduleApiUrl.value, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				date: newDate.value,
+				menu: newMenu.value,
+			}),
+		});
+
+		if (response.ok) {
+			newDate.value = "";
+			newMenu.value = [];
+			fetchDailyMenus();
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Add a new item to the menu.
+	 */
+	const addItem = () => {
+		if (!newItems.value.name || !newItems.value.price) {
+			alert("Vyplňte všechny údaje položky.");
+			return;
+		}
+
+		try {
+			newItems.value.price = parseFloat(newItems.value.price);
+		} catch (e) {
+			alert("Cena musí být číslo.");
+			return;
+		}
+
+		newMenu.value.push(newItems.value);
+		newItems.value = { name: "", price: null };
+	};
+
+	/**
+	 * Delete a menu by its ID.
+	 * @async
+	 * @param {string} id - The ID of the menu to delete.
+	 */
+	const deleteMenu = async (id) => {
+		if (!confirm("Opravdu chcete smazat toto menu?")) {
+			return;
+		}
+
+		const response = await authFetch(moduleApiUrl.value + `/${id}`, {
+			method: "DELETE",
+		});
+
+		if (response.ok) {
+			fetchDailyMenus();
+		} else {
+			alert("Něco se pokazilo.");
+		}
+	};
+
+	/**
+	 * Fetch daily menus on component mount.
+	 */
+	onMounted(() => {
+		if (apiUrl.value) {
+			moduleApiUrl.value = apiUrl.value + "/api/infinityframe/dailyMenu";
+			fetchDailyMenus();
+		}
+	});
 </script>
 
 <template>
@@ -166,7 +166,7 @@ onMounted(() => {
 				<ColumnsLayout :columns="3">
 					<TextInput
 						v-model="newItems.name"
-						placeholder="Věc"
+						placeholder="Položka"
 						icon="lunch_dining"
 					/>
 					<TextInput
@@ -174,11 +174,7 @@ onMounted(() => {
 						placeholder="Cena"
 						icon="payments"
 					/>
-					<Button
-						@click="addItem()"
-						label="Přidat položku"
-						icon="add"
-					/>
+					<Button @click="addItem()" label="Přidat položku" icon="add" />
 				</ColumnsLayout>
 			</Box>
 			<Actions>
@@ -213,16 +209,16 @@ onMounted(() => {
 </template>
 
 <style>
-@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
-@import "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
+	@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+	@import "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
 
-.box.today-true {
-	background-color: #e5ffda;
-}
+	.box.today-true {
+		background-color: #e5ffda;
+	}
 
-* {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
-}
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
 </style>
